5c0cdab212eec95140dcc5879babd77eef24a1d4
AyhanAghayev <agayevayhan5@gmail.com>
2026-01-13 18:51:40 +0400

    NEW VERSIONgit commit -mgit commit -mgit commit -m!

--- minecraft_download.sh
+++ minecraft_download.sh
@@ -5,7 +5,7 @@ wget https://download.oracle.com/java/25/latest/jdk-25_linux-x64_bin.tar.gz -O ~
 mkdir ~/mc-java
 tar -xvf ~/java_tar -C ~/mc-java --strip-components=1
 
-wget https://skmedix.pl/binaries/skl/3.2.12/SKlauncher-3.2.12.jar -O ~/minecraft.jar
+wget https://skmedix.pl/binaries/skl/3.2.18/SKlauncher-3.2.18.jar -O ~/minecraft.jar
 
 wget https://files.catbox.moe/2tokni.zip -O ~/mods.zip
 unzip ~/mods.zip -d ~/
